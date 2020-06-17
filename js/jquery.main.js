6a029b6545b7f2543417d8c5dd23f607283e5f55
Faran Ahmad <faran_ahmad86@yahoo.com>
2020-06-17 12:01:00 -0700

    batch-4

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -1,31 +1,41 @@
 $(document).ready(function(){
-   $('.video-slider').slick();
-   $('.logo-slider').slick({
-   		slidesToShow: 8,
+	var dirVal = $('html').attr('dir');
+	var dir = false;
+	if(dirVal == "rtl"){
+		dir = true;
+	}
+
+	$('.video-slider').slick({
+		rtl: dir
+	});
+
+	$('.logo-slider').slick({
+			slidesToShow: 8,
 		slidesToScroll: 1,
 		autoplay: true,
+		rtl: dir,
 		responsive: [
-	    {
-	      breakpoint: 768,
-	      settings: {
-	        slidesToShow: 5,
-	        slidesToScroll: 1,
-	      }
-	    },
-	    {
-	      breakpoint: 570,
-	      settings: {
-	        slidesToShow: 3,
-	        slidesToScroll: 1
-	      }
-	    },
-	    {
-	      breakpoint: 20,
-	      settings: {
-	        slidesToShow: 1,
-	        slidesToScroll: 1
-	      }
-	    }
-	  ]
-   });
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 5,
+					slidesToScroll: 1,
+				}
+			},
+			{
+				breakpoint: 570,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 20,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
+	 });
 });
